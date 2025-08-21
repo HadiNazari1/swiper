d98219c962763981205bb28e89f6027387428f61
Hadi <41499272+HadiNazari1@users.noreply.github.com>
2025-08-21 09:36:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from time import sleep
 
-FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
-FB_PASSWORD = YOUR FACEBOOK PASSWORD
+FB_EMAIL = "nabi.orfani@yahoo.com"
+FB_PASSWORD = "23454455"  
 
-chrome_driver_path = YOUR CHROME DRIVER PATH
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+service = Service(r"C:\Developers\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 15)
 
-driver.get("http://www.tinder.com")
+driver.get("https://tinder.com")
 
-sleep(2)
-login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
-login_button.click()
 
-sleep(2)
-fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
-fb_login.click()
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="q1862456778"]/div/div[1]/div/main/div[1]/div/div/div/div/div[1]/header/div/div[2]/div[2]/a/div[2]/div[2]/div'))).click()
 
-sleep(2)
-base_window = driver.window_handles[0]
-fb_login_window = driver.window_handles[1]
-driver.switch_to.window(fb_login_window)
-print(driver.title)
 
-email = driver.find_element_by_xpath('//*[@id="email"]')
-password = driver.find_element_by_xpath('//*[@id="pass"]')
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="q134075702"]/div/div[1]/div/div[2]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div[2]/div/div'))).click()
+
+
+wait.until(lambda d: len(d.window_handles) > 1)
+driver.switch_to.window(driver.window_handles[1])
+
+# Enter Facebook login
+email = wait.until(EC.presence_of_element_located((By.ID, "email")))
+password = driver.find_element(By.ID, "pass")
 
 email.send_keys(FB_EMAIL)
 password.send_keys(FB_PASSWORD)
 password.send_keys(Keys.ENTER)
 
-driver.switch_to.window(base_window)
-print(driver.title)
+# Switch back to Tinder
+driver.switch_to.window(driver.window_handles[0])
 
-sleep(5)
-allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-allow_location_button.click()
-notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
-notifications_button.click()
-cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
-cookies.click()
+# Handle popups
+try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Allow")]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(),"Not interested")]'))).click()
+except:
+    pass
 
+# Like loop
 for n in range(100):
-    sleep(1)
+    sleep(2)
     try:
-        print("called")
-        like_button = driver.find_element_by_xpath(
-            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Like"]')))
         like_button.click()
+        print(f"Liked {n+1}")
     except ElementClickInterceptedException:
         try:
-            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup = driver.find_element(By.CSS_SELECTOR, ".itsAMatch a")
             match_popup.click()
         except NoSuchElementException:
             sleep(2)
